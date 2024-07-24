a099a9c3511da9d569eeaeb1f448e41066fbe7a8
Ajinkya G <127004966+BlazeBolt@users.noreply.github.com>
2024-07-24 10:12:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.querySelector('.toggle-button');
-    const navbarLinks = document.querySelector('.navbar-links');
 
-    toggleButton.addEventListener('click', function () {
-        navbarLinks.classList.toggle('active');
-    });
-});
